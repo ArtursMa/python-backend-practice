8c4f82588e7ddcc8e82e87854583f1b9b9b923c1
ArtursMakna <72070977+ArtursMa@users.noreply.github.com>
2026-07-16 15:16:23 +0300

    Validate client phone number

--- appointment.py
+++ appointment.py
@@ -12,6 +12,13 @@ class Client:
         if not stripped_name:
             raise ValueError("Please write client name")
         self.name = stripped_name
+        if not isinstance(self.phone, str):
+            raise TypeError("Phone should be string")
+        stripped_phone = self.phone.strip()
+        if not stripped_phone:
+            raise ValueError("Please write a number")
+        self.phone = stripped_phone
+
 
 
 try:
@@ -20,3 +27,11 @@ try:
 except ValueError as ex:
     print(ex)
 print(Client(" Anna ", "+22323822"))
+try:
+    Client("Valery", 3333)
+except TypeError as ex:
+    print(ex)
+try:
+    Client("sds", " ")
+except ValueError as ex:
+    print(ex)
