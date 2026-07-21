a30f6250ff36ecf031d0ef1641c4e5881db60ab7
ArtursMakna <72070977+ArtursMa@users.noreply.github.com>
2026-07-21 10:21:28 +0300

    Create appointment class

--- appointment.py
+++ appointment.py
@@ -1,6 +1,18 @@
 from dataclasses import dataclass
+from datetime import timedelta
+
+from Client import Client
 
 
 @dataclass
 class Appointment:
-    pass
+    client: Client
+    service_name: str
+    client_hours: str
+    price_in_cents: int
+    is_confirmed: bool
+
+
+first_client = Client("Anna","+322323232")
+first_appointment = Appointment(first_client,"Electrolysis", "2", 60, True)
+
