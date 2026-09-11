11767b10e7d13edae1ab9c34f3958e1ddf778ece
ArtursMakna <72070977+ArtursMa@users.noreply.github.com>
2026-09-11 22:04:34 +0300

    add update client by id function

--- database.py
+++ database.py
@@ -5,7 +5,7 @@ def get_connection():
     return sqlite3.connect("beauty.db")
 
 
-def get_all_clients():  
+def get_all_clients():
     beauty_database_connection = get_connection()
     clients_cursor = beauty_database_connection.execute("SELECT * FROM clients")
     all_clients_data = clients_cursor.fetchall()
@@ -22,20 +22,29 @@ def get_client_by_id(client_id):
 
 
 def add_client(name, phone, status):
-
     connection = get_connection()
     with connection:
-        beauty_db_cursor = connection.execute("INSERT INTO clients(name,phone,status) VALUES(?,?,?)", (name, phone, status))
+        beauty_db_cursor = connection.execute("INSERT INTO clients(name,phone,status) VALUES(?,?,?)",
+                                              (name, phone, status))
         last_row_id = beauty_db_cursor.lastrowid
 
     connection.close()
     return last_row_id
 
 
+def update_client(updated_id, name, phone, status):
+    is_updated = False
+    connection = get_connection()
+    with connection:
+        beauty_db_cursor = connection.execute("UPDATE clients SET name = ?,phone = ?,status = ? WHERE id =?",
+                                              (name, phone, status, updated_id))
+        if beauty_db_cursor.rowcount:
+            is_updated = True
+        
+
+    connection.close()
+    return is_updated
 
 
-
-
-
-
+print(update_client(0, "Elene", "555", 0))
 
